237b9a41f82b0c979a2516c77bab89cc4a122eb0
Manideep Naidu <manideepnaidugorle@gmail.com>
2025-07-03 00:52:32 +0530

    add logo and select box

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <>
         <h1>Welcome to Pizza Shop</h1>
-         <Button variant='link'>Click me</Button>
-    </div>
+         <Button>Click me</Button>
+    </>
   );
 }
